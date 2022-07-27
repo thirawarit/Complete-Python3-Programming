f050554bc1645b116f5202e397bd062bf3e429ed
Thirawarit.p <Thirawarit.p@gmail.com>
2022-07-27 23:08:32 +0700

    Create Test-Float.py

---
+++ Test-Float.py
@@ -0,0 +1,8 @@
+#Test-Float
+if __name__ == '__main__' :
+    print(3.14," is type of ", type(3.14))
+    print(10+3.14, " is type of ", type(10+3.14))
+    print(10+3.0, " is type of ", type(10+3.0))
+    print(10-3.0, " is type of ", type(10-3.0))
+    print(5*1.0, " is type of ", type(5*1.0))
+    print(15.1*2, " is type of ", type(15.1*2))
